e1e15d0168062c449ccb4b5a0697155774792e5f
GiPisano <pisanogiacomo9@gmail.com>
2024-04-07 12:16:49 +0200

    add type models and the relationship with the projects

--- database/migrations/2024_04_03_084500_create_types_table.php
+++ database/migrations/2024_04_03_084500_create_types_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('type_id')->constrained();
-            $table->string('title', 100);
-            $table->text('description');
-            $table->string('author', 50);
+            $table->string('label', 30);
+            $table->char('color', 7);
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('types');
     }
 };
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -16,8 +17,11 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $types_id = Type::all()->pluck('id');
+
         for($i = 0; $i < 25; $i++){
             $project = new project;
+            $project->type_id = $faker->randomElement($types_id);
             $project->title = $faker->sentence();
             $project->description = $faker->paragraph();
             $project->author = $faker->name();
